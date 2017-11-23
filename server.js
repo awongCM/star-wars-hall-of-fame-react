6280779d9d82da9795fc84fb1bf054a0c49aaa18
awongCM <awong.cm@gmail.com>
2017-11-23 16:13:20 +1100

    removed unwanted code base;

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const express = require('express'),
-	  request = require('request'),
-	  bodyParser = require('body-parser'),
-	  methodOverride = require('method-override'),
-	  fs = require('fs');
+// --- NOT LONGER REQUIRED
+// 
+// const express = require('express'),
+// 	  request = require('request'),
+// 	  bodyParser = require('body-parser'),
+// 	  methodOverride = require('method-override'),
+// 	  fs = require('fs');
 
-const app = express();
+// const app = express();
 
-app.set("port", process.env.PORT || 3001);
+// app.set("port", process.env.PORT || 3001);
 
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use(methodOverride());
+// app.use(methodOverride());
 
-app.get("/api/people", (req, res) => {
+// app.get("/api/people", (req, res) => {
     
-})
+// })
 
-app.listen(app.get("port"), () => {
-  console.log(`Server listening on ${app.get("port")}`);
-})
+// app.listen(app.get("port"), () => {
+//   console.log(`Server listening on ${app.get("port")}`);
+// }
