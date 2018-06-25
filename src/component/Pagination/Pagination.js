1dc30be77a16bb6ab723605dc54eef4c889ae20e
awongCM <awong.cm@gmail.com>
2018-06-25 20:40:55 +1000

    added simple pagination logic to revise

--- src/component/Pagination/Pagination.js
+++ src/component/Pagination/Pagination.js
@@ -14,18 +14,39 @@ export class Pagination extends Component {
       firstPage: (this.props.paginationData.current_page === 1),
       nextPage: this.props.paginationData.current_page + 1,
       finalPage: this.props.paginationData.current_page === this.props.paginationData.total_pages - 1,
+      currentPage: this.props.paginationData.current_page
     };
   }
 
   handlePreviousReq() {
     if(this.state.allowPrevious && this.state.previousPage > 0){
       console.log('go previous page');
+      const newCurrentPage = --this.state.currentPage;
+      const newPreviousPage = newCurrentPage - 1;
+      const newNextPage = newCurrentPage +1;
+      this.setState({
+        currentPage: newCurrentPage, 
+        previousPage: newPreviousPage, 
+        nextPage: newNextPage,
+        allowPrevious: (newCurrentPage !== 1),
+        allowNext: (newCurrentPage !== this.props.paginationData.total_pages)
+      });
     }
   }
 
   handleNextReq() {
     if (this.state.allowNext && this.state.nextPage < this.props.paginationData.total_count) {
       console.log('go next page');
+      const newCurrentPage = ++this.state.currentPage;
+      const newPreviousPage = newCurrentPage - 1;
+      const newNextPage = newCurrentPage +1;
+      this.setState({
+        currentPage: newCurrentPage, 
+        previousPage: newPreviousPage, 
+        nextPage: newNextPage,
+        allowPrevious: (newCurrentPage !== 1),
+        allowNext: (newCurrentPage !== this.props.paginationData.total_pages)
+      });
     }
   }
 
@@ -33,8 +54,8 @@ export class Pagination extends Component {
     return (
       <nav aria-label="Page navigation">
         <ul className="pagination flex-center">
-          <li className="page-item" onClick={this.handleNextReq.bind(this)}>
-            <a className="page-link" href="#"> &larr; Prev</a>  
+          <li className="page-item" onClick={this.handlePreviousReq.bind(this)}>
+            <a className="page-link"> &larr; Prev</a>  
           </li>
           <li className="page-item">
             <div className="inline-pagination">
@@ -48,11 +69,14 @@ export class Pagination extends Component {
                 this.props.paginationData.total_count :
                 this.props.paginationData.current_page *  this.props.paginationData.max_per_page
               }
+              <em>
+                Page No: {this.state.currentPage}
+              </em>
               <strong>Total Results: {this.props.paginationData.total_count}</strong>
             </div>
           </li>
           <li className="page-item" onClick={this.handleNextReq.bind(this)}>
-              <a className="page-link" href="#">Next &rarr;</a>
+              <a className="page-link">Next &rarr;</a>
           </li>
         </ul>
       </nav>
