7e25dc8dafaffbf892ccc52dc0379b843f8183d7
awongCM <awong.cm@gmail.com>
2018-07-03 15:53:57 +1000

    added select dropdown; commented todos; reset input styles

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -12,6 +12,12 @@ class Form extends Component {
 			<div className="formContainer">
 	          <form action="" id="form" className="formContainer--formControl">
 	            <label htmlFor="inputControl" className="formContainer--searchLabel">Filter By Character Name:</label>
+							{/* TODO - select dropdown */}
+							<select className="formContainer--selectControl">
+								<option>Character</option> 
+								<option>Planet</option> 
+								<option>Movie</option>
+							</select>
 	            <input type="text" id="inputControl" className="formContainer--textControl" onChange={this.handleCharacterFilter.bind(this)} />
 	          </form>
 	        </div>
