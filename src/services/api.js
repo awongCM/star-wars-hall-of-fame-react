b23f4183f1ce7049eba52d76d8197c1cf75ead80
awongCM <awong.cm@gmail.com>
2020-03-12 21:02:13 +1300

    added api todos

--- src/services/api.js
+++ src/services/api.js
@@ -1,35 +1,38 @@
 //API Request Settings
 
+// TODOS - other urls here
+// Planet - https://swapi.co/api/planets/
+// Movie - https://swapi.co/api/films/
+// Starship - https://swapi.co/api/starships/
+
 export const url = `https://swapi.co/api/people/`;
 
 export function initHeaders() {
-	return {
-		method: 'GET',
-		cache: 'default',
-		accept: 'application/json'
-	}
+  return {
+    method: "GET",
+    cache: "default",
+    accept: "application/json"
+  };
 }
 
 export function requestURL(requested_URL) {
-	return new Promise( function (resolve, reject) {
-		_requestURL(requested_URL)
-			.then((res) =>  resolve(res))
-			.catch((err) => reject(err));
-	});
+  return new Promise(function(resolve, reject) {
+    _requestURL(requested_URL)
+      .then(res => resolve(res))
+      .catch(err => reject(err));
+  });
 }
 
 export function requestURLs(requested_URLs) {
-	return requested_URLs.map( (requested_URL) => requestURL(requested_URL));
+  return requested_URLs.map(requested_URL => requestURL(requested_URL));
 }
 
 async function _requestURL(requested_URL) {
-	try {
-
-		let response = await fetch(requested_URL, initHeaders());
-		const resp_json = await response.json();
-		return resp_json;
-
-	} catch (error) {
-		return error;
-	}
+  try {
+    let response = await fetch(requested_URL, initHeaders());
+    const resp_json = await response.json();
+    return resp_json;
+  } catch (error) {
+    return error;
+  }
 }
