09d144320b3eb49695f280e8b575fe751d42d49d
awongCM <awong.cm@gmail.com>
2022-12-05 22:39:25 +1100

    refactor landing page to use hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
-import {Router, Route, browserHistory } from "react-router";
+import React from "react";
+import { Router, Route, browserHistory } from "react-router";
 import MainApp from "./component/MainApp/MainApp";
 import Home from "./component/Home/Home";
 import Detail from "./component/Detail/Detail";
-import DetailPlanet from './component/DetailPlanet/DetailPlanet';
-import DetailFilm from './component/DetailFilm/DetailFilm';
+import DetailPlanet from "./component/DetailPlanet/DetailPlanet";
+import DetailFilm from "./component/DetailFilm/DetailFilm";
 
-class App extends Component {
-
-  render() {
-
-    return (
-      <Router history={browserHistory}>
-        <Route component={MainApp} >
-          <Route path="/" component={Home} />
-          <Route path="/people/:id" component={Detail}  />
-          <Route path="/planet/:id" component={DetailPlanet}/>
-          <Route path="/film/:id" component={DetailFilm} />
-        </Route>
-      </Router>
-    );
-  }
-}
-
+const App = () => {
+  return (
+    <Router history={browserHistory}>
+      <Route component={MainApp}>
+        <Route path="/" component={Home} />
+        <Route path="/people/:id" component={Detail} />
+        <Route path="/planet/:id" component={DetailPlanet} />
+        <Route path="/film/:id" component={DetailFilm} />
+      </Route>
+    </Router>
+  );
+};
 
 export default App;
